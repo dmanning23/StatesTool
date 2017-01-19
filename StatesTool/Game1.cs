97cbc258216d6ef76bfa972fb988ad356f05b86e
Dan Manning <dmanning23@hotmail.com>
2017-01-18 22:50:50 -0600

    initial commit project structure

---
+++ StatesTool/Game1.cs
@@ -0,0 +1,70 @@
+﻿//using AnimationTool.Screens;
+using InputHelper;
+using MenuBuddy;
+using Microsoft.Xna.Framework;
+
+namespace StatesTool
+{
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
+	//public class Game1 : ControllerGame
+
+#if __IOS__ || ANDROID || WINDOWS_UAP
+	public class Game1 : TouchGame
+#else
+	public class Game1 : MouseGame
+#endif
+	{
+		#region Properties
+
+		//AnimationManager AnimationManager { get; set; }
+
+		#endregion //Properties
+
+		#region Methods
+
+		public Game1()
+		{
+			//FullScreen = true;
+
+			//AnimationManager = new AnimationManager(this);
+
+			var debug = new DebugInputComponent(this, ResolutionBuddy.Resolution.TransformationMatrix);
+			debug.DrawOrder = 1000;
+		}
+
+		protected override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		protected override void Initialize()
+		{
+			//AnimationManager.LoadContent();
+			base.Initialize();
+		}
+
+		protected override void InitStyles()
+		{
+			StyleSheet.SmallFontResource = @"Fonts\ArialBlack10";
+			base.InitStyles();
+
+			//DefaultStyles.Instance().MainStyle.HasOutline = true;
+			//DefaultStyles.Instance().MenuEntryStyle.HasOutline = true;
+			//DefaultStyles.Instance().MenuTitleStyle.HasOutline = true;
+			//DefaultStyles.Instance().MessageBoxStyle.HasOutline = true;
+		}
+
+		/// <summary>
+		/// Get the set of screens needed for the main menu
+		/// </summary>
+		/// <returns>The gameplay screen stack.</returns>
+		public override IScreen[] GetMainMenuScreenStack()
+		{
+			return new IScreen[] { new ModelScreen(AnimationManager), new ToolsScreen(AnimationManager) };
+		}
+
+		#endregion //Methods
+	}
+}
