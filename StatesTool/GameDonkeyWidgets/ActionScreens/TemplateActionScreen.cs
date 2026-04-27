bfdba54f11306450314423d6aacc4bf437e0bae2
Dan Manning <1888089+dmanning23@users.noreply.github.com>
2026-04-26 22:53:12 -0500

    Added templateaction

---
+++ StatesTool/GameDonkeyWidgets/ActionScreens/TemplateActionScreen.cs
@@ -0,0 +1,28 @@
+﻿using GameDonkeyLib;
+
+namespace StatesTool
+{
+    public class TemplateActionScreen : BaseActionScreen
+    {
+        #region Methods
+
+        public TemplateActionScreen(BaseAction stateAction, IPlayerQueue character) : base("Template Action", stateAction, character)
+        {
+        }
+
+        protected override void AddStateActionWidgets()
+        {
+            var action = StateAction as TemplateAction;
+
+            var button = CreateButton("Actions", ToolStack);
+            button.OnClick += Button_OnClick;
+        }
+
+        private void Button_OnClick(object sender, InputHelper.ClickEventArgs e)
+        {
+            ScreenManager.AddScreen(new StateActionsScreen(Engine, Character, StateAction as TemplateAction, "Template Actions"));
+        }
+
+        #endregion //Methods
+    }
+}
